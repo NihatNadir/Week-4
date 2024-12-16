eec48e6ed3821be9929a4993bec77f31d07f7ed2
NihatNadir <nihat.nadir.borlu@outlook.com>
2024-12-17 00:42:35 +0300

    Landing-Page added

---
+++ Landing-Page/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    extend: {
+      colors: {
+        'bg-color': '#F2F3F5',
+        'btn-color':'#7C3AEC',
+        'text-color':'#111827',
+        'icon-color':'#4B5563',
+        'div-color':'#F9FAFB',
+      },
+    },
+  },
+  plugins: [],
+}
