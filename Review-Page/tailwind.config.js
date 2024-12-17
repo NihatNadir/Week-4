340ac58e078a1ca09e8e20eeed447d6858b1561f
NihatNadir <nihat.nadir.borlu@outlook.com>
2024-12-18 00:45:11 +0300

    Review-Page added

---
+++ Review-Page/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bg-color': '#F3F4F6',
+        'btn-color':'#7C3AEC',
+        'text-color':'#111827',
+        'placeholder':'#F3F4F6',
+        'div-color':'#D1D5DB',
+      },
+    },
+  },
+  plugins: [],
+}
